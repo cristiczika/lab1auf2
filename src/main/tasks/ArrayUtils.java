91f38914f74137d2b3c7229067da581d4d7ae578
cristiczika <cristianczika6@gmail.com>
2025-10-20 17:39:13 +0300

    1)

--- src/main/tasks/ArrayUtils.java
+++ src/main/tasks/ArrayUtils.java
@@ -1,4 +1,17 @@
 package main.tasks;
 
 public class ArrayUtils {
+
+    public static int getMax(int[] numbers) {
+        int max = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        return max;
+    }
+
 }
