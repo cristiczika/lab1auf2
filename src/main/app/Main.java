5c71a91a9b12149d06ca501c6940027f0140dcbe
cristiczika <cristianczika6@gmail.com>
2025-10-20 17:42:31 +0300

    Cleanup

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -5,7 +5,7 @@ import main.tasks.ArrayUtils;
 public class Main {
 
     static void main() {
-        IO.println("------- Aufgabe 2 --------");
+        IO.println("\n------- Aufgabe 2 --------");
         int[] numbers = {4, 8, 3, 10, 17};
         IO.println(ArrayUtils.getMax(numbers));
         IO.println(ArrayUtils.getMin(numbers));
