1756a442aea4cb42f005860d0b32335cb0eee285
cristiczika <cristianczika6@gmail.com>
2025-10-20 17:41:34 +0300

    Main

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -1,9 +1,16 @@
 package main.app;
 
+import main.tasks.ArrayUtils;
+
 public class Main {
 
     static void main() {
-
+        IO.println("------- Aufgabe 2 --------");
+        int[] numbers = {4, 8, 3, 10, 17};
+        IO.println(ArrayUtils.getMax(numbers));
+        IO.println(ArrayUtils.getMin(numbers));
+        IO.println(ArrayUtils.getMaxSum(numbers));
+        IO.println(ArrayUtils.getMinSum(numbers));
     }
 
 }
